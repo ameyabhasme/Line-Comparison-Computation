db311c5d5840cd7663a70babe4108b5eca6c9283
ameyabhasme <ameyabhasme5@gmail.com>
2021-07-31 17:40:49 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/LineComparisonComputation/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.LineComparisonComputation;
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Problem");
+	}
+}
